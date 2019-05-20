b18238b510f5b8e74cd30e4e774ea2ae50693ecd
doadet <davidpom@gmail.com>
2019-05-20 15:05:19 -0400

    more conditional changes, and getting the game to run over again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,44 +8,52 @@ var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
 
 
 
-    // if(i == 3)
-    //     continue;
 
-    // alert(i);
-
-    function rand (alpha) {
+function rand (alpha) {
 	return alpha[Math.floor(Math.random()* alpha.length)];
+};
+
+var compChoice = rand(alpha);
+console.log(compChoice);
+
+document.onkeyup = function(event) {
+
+        var userGuess = event.key;
+
+        var computerGuess = alpha[Math.floor(Math.random() * alpha.length)];
+                    
+        if (alpha.indexOf(userGuess) > -1) {
+
+            if (userGuess === compChoice) {  
+                wins++;
+                alert("you guessed right: " + userGuess );
+                turns = 9;
+                lettersChosen = [];
+                compChoice = rand(alpha);
+                console.log("comp guess again: " + compChoice)
+            }
+
+            if (userGuess != compChoice) {
+                turns --;
+                lettersChosen.push(userGuess);
+            }
+
+            if (turns === 0) {            
+                turns = 9;
+                losses ++;
+                lettersChosen = [];
+                alert("out of guesses");
+                compChoice = rand(alpha);
+                console.log("comp guess again2: " + compChoice)                
+            }   
+        
+
+        document.querySelector("#wins").innerHTML = "wins: " + wins;
+        document.querySelector("#losses").innerHTML = "losses: " + losses;
+        document.querySelector("#turns").innerHTML = "turns: " + turns;
+        document.querySelector("#lettersChosen").innerHTML = "already: " + lettersChosen;
+
+
+        
+        }
     };
-
-    var compChoice = rand(alpha);
-    console.log(compChoice);
-
-    document.onkeyup = function(event) {
-
-        var letter = String.fromCharCode(event.keyCode).toLowerCase();
-        for(var i = 0; i < 9;) {
-        if (letter == compChoice) {
-
-            alert("correct", lettersChosen);
-            document.getElementById("wins").innerHTML = (wins.length);
-            lettersChosen.push(letter);
-            document.getElementById("lettersChosen").innerHTML=(lettersChosen);
-            (wins+=1);
-            i == 5;
-    
-
-        }
-        else {
-
-            console.log("no", compChoice, letter);
-            document.getElementById("losses").innerHTML= (losses.length);
-            lettersChosen.push(letter);
-            document.getElementById("lettersChosen").innerHTML=(lettersChosen);
-            turns--;
-
-            document.getElementById("compChoice").innerHTML=(compChoice);
-            i == i++;
-        }
-        }
-        // if (letter == compChoice)
-    }
